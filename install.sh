4948c6484cbb43287ddf672b6fb8c6c88b817ff6
Ryu236 <ryu.adv.2360@gmail.com>
2019-06-13 16:14:32 +0900

    Add Brewfile and add Brewfile in install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 
 for f in .??*
 do 
-	[[ "$f" == ".git" ]] && continue
-	[[ "$f" == ".DS_Store" ]] && continue
-	ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
-	echo "$f"
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+  ln -snfv ${dot_directory}/${f} ${home}/${f}
+  ln -snfv ${dot_directory}/Brewfile ${home}/Brewfile
+  echo "$f"
 done
