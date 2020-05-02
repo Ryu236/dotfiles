65aa3e120fdb1cd7a5df94c92687387ea38f3e8a
Ryu236 <ryu.adv.2360@gmail.com>
2020-05-03 01:13:43 +0900

    :sparkles: Feat(git): add a script to set up git config

---
+++ install-gitconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set up git config script
+git config --global alias.b branch
+git config --global alias.c checkout
+git config --global alias.s status
+git config --global alias.alias "!git config --get-regexp '^alias\\.' | sed 's/alias\\.\\([^ ]*\\) \\(.*\\)/\\1\\	 => \\2/' | sort"
+git config --global alias.refresh "!git fetch origin && git remote prune origin"
+git config --global alias.d1 "diff HEAD~"
+git config --global alias.d2 "diff HEAD~2"
+git config --global alias.d3 "diff HEAD~3"
