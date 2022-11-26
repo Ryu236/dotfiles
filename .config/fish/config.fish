861077177b5f310bf1eae55626e7baa4ccaafc46
Ryu236 <ryu.adv.2360@gmail.com>
2022-11-26 12:24:30 +0900

    :sparkles: feat(fish): Add fish cofig file

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+set -gx TERM xterm-256color
+
+# aliases
+alias ll "exa -l -g --icons"
+alias la "ll -a"
+alias vim "nvim"
+alias g "git"
+alias cat "bat"
+alias gl "git log --oneline"
+
+set -gx EDITOR nvim
+
+set -xU PATH $HOME/flutter/bin:$PATH
+set -xU PATH $HOME/.rbenv/bin:$PATH
+eval $(rbenv init - | source)
+set -U nvm_default_version v16.14.0
+set -xU ANDROID_SDK_ROOT $HOME/Library/Android/sdk
+set -xU PATH $ANDROID_SDK_ROOT/emulator
+set -xU PATH $ANDROID_SDK_ROOT/platform-tools
+set -x JAVA_HOME $(/usr/libexec/java_home -v "11")
+set -xU ANDROID_HOME $HOME/Library/Android/sdk
+set -xU PATH $PATH:$ANDROID_HOME/tools
+set -xU PATH $PATH:$ANDROID_HOME/tools/bin
+set -xU PATH $PATH:$ANDROID_HOME/platform-tools
