600a9e76425fa2b8839f4496c162287f7f4cdb5c
Ryu236 <ryu.adv.2360@gmail.com>
2023-05-12 15:09:55 +0900

    :lipstick: style(nvim): Change colorscheme of background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,6 +132,11 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'iceberg'
+      vim.cmd.highlight 'Normal ctermbg=NONE guibg=NONE'
+      vim.cmd.highlight 'NonText ctermbg=NONE guibg=NONE'
+      vim.cmd.highlight 'LineNr ctermbg=NONE guibg=NONE'
+      vim.cmd.highlight 'Folded ctermbg=NONE guibg=NONE'
+      vim.cmd.highlight 'EndOfBuffer ctermbg=NONE guibg=NONE'
     end,
   },
 
