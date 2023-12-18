7a64ead6a107f4b470f98f5656bc64357845d562
Ryu236 <ryu.adv.2360@gmail.com>
2023-12-18 22:55:54 +0900

    feat(nvim): Add keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- [[ Basic Keymaps ]]
 
+local opts = { noremap = true, silent = true }
+
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -13,3 +15,28 @@ vim.keymap.set('n', 'sh', '<C-w>h', { desc = 'move window left' })
 vim.keymap.set('n', 'sj', '<C-w>j', { desc = 'move window down' })
 vim.keymap.set('n', 'sk', '<C-w>k', { desc = 'move window up' })
 vim.keymap.set('n', 'sl', '<C-w>l', { desc = 'move window right' })
+
+-- Increment/decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+vim.keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- New tab
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Resize window
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
